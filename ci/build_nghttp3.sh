0809fbb302b653c865b21beafc27beaf7a1dfaed
liuchuan-c <liuchuan-c@360.cn>
2019-07-26 14:06:26 +0800

    ci: build pass for the draft-20

--- ci/build_nghttp3.sh
+++ ci/build_nghttp3.sh
@@ -4,6 +4,8 @@
 cd ..
 git clone https://github.com/ngtcp2/nghttp3
 cd nghttp3
+# for draft-20
+git checkout a46f2898d6faf8
 autoreconf -i
 ./configure --prefix=$PWD/build --enable-lib-only
 make -j$(nproc) check
